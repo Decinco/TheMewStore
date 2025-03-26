222018ac70c1abe48de3bf8e127f9d97f46f88b0
teropod24 <jx.paredes@sarria.salesians.cat>
2025-03-26 17:51:32 +0100

    crear el modelo de shoppingcart

--- themewstore/lib/app/routes/app_pages.dart
+++ themewstore/lib/app/routes/app_pages.dart
@@ -4,10 +4,12 @@ import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
-import '../modules/signup/bindings/signup_binding.dart';
-import '../modules/signup/views/signup_view.dart';
 import '../modules/product/bindings/product_binding.dart';
 import '../modules/product/views/product_view.dart';
+import '../modules/shoppingcart/bindings/shoppingcart_binding.dart';
+import '../modules/shoppingcart/views/shoppingcart_view.dart';
+import '../modules/signup/bindings/signup_binding.dart';
+import '../modules/signup/views/signup_view.dart';
 
 part 'app_routes.dart';
 
@@ -42,6 +44,11 @@ class AppPages {
       name: _Paths.PRODUCT,
       page: () => ProductView(),
       binding: ProductBinding(),
+    ),
+    GetPage(
+      name: _Paths.SHOPPINGCART,
+      page: () => const ShoppingcartView(),
+      binding: ShoppingcartBinding(),
     ),
   ];
 }
