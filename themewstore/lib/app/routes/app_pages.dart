0eee5e74708d46828bcfca964cca248e4472d4b0
Decinco <danielmug.dm@gmail.com>
2025-03-31 18:06:31 +0200

    nada

--- themewstore/lib/app/routes/app_pages.dart
+++ themewstore/lib/app/routes/app_pages.dart
@@ -4,10 +4,12 @@ import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
-import '../modules/signup/bindings/signup_binding.dart';
-import '../modules/signup/views/signup_view.dart';
+import '../modules/password/bindings/password_binding.dart';
+import '../modules/password/views/password_view.dart';
 import '../modules/product/bindings/product_binding.dart';
 import '../modules/product/views/product_view.dart';
+import '../modules/signup/bindings/signup_binding.dart';
+import '../modules/signup/views/signup_view.dart';
 
 part 'app_routes.dart';
 
@@ -42,6 +44,11 @@ class AppPages {
       name: _Paths.PRODUCT,
       page: () => ProductView(),
       binding: ProductBinding(),
+    ),
+    GetPage(
+      name: _Paths.PASSWORD,
+      page: () => const PasswordView(),
+      binding: PasswordBinding(),
     ),
   ];
 }
