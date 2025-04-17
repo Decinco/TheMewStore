b683c9320f0f3328dac28fcd88e479677e78aa9f
Daniel Mugueta <103062948+Decinco@users.noreply.github.com>
2025-04-17 12:48:39 +0200

    perfil se muestra

--- lib/app/modules/profilefriends/views/profilefriends_view.dart
+++ lib/app/modules/profilefriends/views/profilefriends_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:themewstore/app/data/models/userData.dart';
+import 'package:themewstore/uicon.dart';
 
 import '../controllers/profilefriends_controller.dart';
 
@@ -9,17 +10,48 @@ class ProfilefriendsView extends GetView<ProfilefriendsController> {
   const ProfilefriendsView({super.key});
 
   Widget profileInfo(UserData userData) {
-    return Column(
-      children: [
-        CircleAvatar(
-          backgroundImage: NetworkImage(userData.profilePicture ??
-              'https://efozsdbswdnjwwgdbmzo.supabase.co/storage/v1/object/public/profilepictures//default-avatar-profile-vector-user-profile-default-avatar-profile-vector-user-profile-profile-179376714-1760973665.jpg'),
-          radius: 50,
-        ),
-        Text(userData.userName ?? 'No Name'),
-        Text(userData.email),
-      ],
-    );
+    return Padding(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.all(5),
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(userData
+                                  .profilePicture ??
+                              'https://efozsdbswdnjwwgdbmzo.supabase.co/storage/v1/object/public/profilepictures//default-avatar-profile-vector-user-profile-default-avatar-profile-vector-user-profile-profile-179376714-1760973665.jpg'),
+                          radius: 50,
+                        )),
+                    Positioned(
+                        right: 3,
+                        top: 3,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          child: Container(
+                            color: Color.fromRGBO(217, 217, 217, 1),
+                            width: 30,
+                            height: 30,
+                            child: IconButton(
+                                padding: EdgeInsets.zero,
+                                onPressed: () {
+                                  Get.snackbar("cambiar Imagen", "TODO");
+                                },
+                                icon: Icon(UIcons.fibspencil)
+                            ),
+                          ),
+                        ))
+                  ],
+                ),
+                Text(userData.userName ?? 'No Name'),
+                Text(userData.email),
+              ],
+            )
+          ],
+        ));
   }
 
   @override
@@ -29,34 +61,58 @@ class ProfilefriendsView extends GetView<ProfilefriendsController> {
           title: const Text('Profile & Friends',
               style: TextStyle(
                 fontSize: 24,
+                fontFamily: "Inter",
                 fontWeight: FontWeight.bold,
               )),
           backgroundColor: Color.fromARGB(255, 237, 213, 229),
           centerTitle: true,
         ),
         body: Container(
-          color: Color.fromARGB(255, 237, 213, 229),
-          width: double.infinity,
-          child: FutureBuilder(
-              future: controller.getProfileData(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData) {
-                  return Center(child: Text('No data found'));
-                } else {
-                  UserData userData = snapshot.data!;
-
-                  return Column(
-                    children: [
-                      profileInfo(userData),
-                      // Add more widgets here as needed
+            color: Color.fromARGB(255, 237, 213, 229),
+            width: double.infinity,
+            child: DefaultTabController(
+                length: 2,
+                child: Column(children: [
+                  TabBar(
+                    tabs: [
+                      Tab(
+                        text: "Profile",
+                        icon: Icon(UIcons.fibsuser),
+                      ),
+                      Tab(
+                        text: "Friends",
+                        icon: Icon(UIcons.fibsusers),
+                      )
                     ],
-                  );
-                }
-              }),
-        ));
+                    labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold, fontFamily: "Inter"),
+                  ),
+                  Expanded(
+                      child: TabBarView(children: [
+                    FutureBuilder(
+                        future: controller.getProfileData(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(child: CircularProgressIndicator());
+                          } else if (snapshot.hasError) {
+                            return Center(
+                                child: Text('Error: ${snapshot.error}'));
+                          } else if (!snapshot.hasData) {
+                            return Center(child: Text('No data found'));
+                          } else {
+                            UserData userData = snapshot.data!;
+
+                            return Column(
+                              children: [
+                                profileInfo(userData),
+                                // Add more widgets here as needed
+                              ],
+                            );
+                          }
+                        }),
+                    Container()
+                  ]))
+                ]))));
   }
 }
