13b42ce24e5d31812eff009fa33a5a1dcafee97a
Leandro Varas <leovaras2005@gmail.com>
2025-04-17 13:23:29 +0200

    feature: menu lateral agregado

--- themewstore/lib/app/modules/home/views/home_view.dart
+++ themewstore/lib/app/modules/home/views/home_view.dart
@@ -22,14 +22,16 @@ class HomeView extends GetView<HomeController> {
           children: [
             const Text('The Mew Store', style: TextStyle(fontWeight: FontWeight.bold)),
             const SizedBox(width: 10),
-            const Icon(UIcons.fibsshoppingbag, color: Colors.black),
+            Image.asset('../../../assets/images/themewstore.png', height: 30),
           ],
         ),
         centerTitle: true,
         backgroundColor: const Color.fromRGBO(237, 213, 229, 1),
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
         ),
         actions: [
           IconButton(
@@ -37,6 +39,39 @@ class HomeView extends GetView<HomeController> {
             onPressed: () {},
           ),
         ],
+      ),
+      drawer: Drawer(
+        backgroundColor: const Color.fromRGBO(237, 213, 229, 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/user.png', width: 50, height: 50),
+            const SizedBox(height: 20),
+            IconButton(
+              icon: Image.asset('assets/pokemon_button.png'),
+              iconSize: 50,
+              onPressed: () {},
+            ),
+            const SizedBox(height: 20),
+            IconButton(
+              icon: Image.asset('assets/stock_button.png'),
+              iconSize: 50,
+              onPressed: () {},
+            ),
+            const SizedBox(height: 20),
+            IconButton(
+              icon: Image.asset('assets/payment_button.png'),
+              iconSize: 50,
+              onPressed: () {},
+            ),
+            const SizedBox(height: 20),
+            IconButton(
+              icon: Image.asset('assets/users_button.png'),
+              iconSize: 50,
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
       body: Column(
         children: [
