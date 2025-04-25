9fc682c1da7e90dfa7b5661282349ba5959af46e
Daniel Mugueta <103062948+Decinco@users.noreply.github.com>
2025-04-25 16:11:16 +0200

    Revert "Feature/home functions"

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:get/get.dart';
 
 import '../controllers/splash_controller.dart';
@@ -11,28 +12,10 @@ class SplashView extends GetView<SplashController> {
       color: const Color.fromARGB(255, 237, 213, 229),
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 237, 213, 229),
-        body: Stack(
-          children: [
-            // Imagen principal centrada
-            Center(
-              child: Image.asset(
-                'assets/images/themewstore/themewstore.png',
-              ),
-            ),
-            // GIF en la parte inferior central
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    bottom:
-                        30.0), // Ajusta la distancia desde el borde inferior
-                child: Image.asset(
-                  'assets/images/themewstore/mew_splash.gif', // Ruta de tu GIF
-                  height: 100.0, // Ajusta el tamaño del GIF según sea necesario
-                ),
-              ),
-            ),
-          ],
+        body: Center(
+          child: Image.asset(
+            'assets/images/themewstore/themewstore.png',
+          ),
         ),
       ),
     );
